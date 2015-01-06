94b3c725841327c08977b2f32a30a4047dd996ac
Nickchooshin <gustncjswp95@gmail.com>
2015-01-06 15:37:45 +0900

    Rancho Eat Animation 추가

--- Project-FW/Friends_Rancho.cpp
+++ Project-FW/Friends_Rancho.cpp
@@ -2,12 +2,18 @@
 #include "Effect_RanchoEat.h"
 #include "Effect_SparkImpact.h"
 #include "Sprite.h"
+#include "LoadManager.h"
 
 #include "Effect_List.h"
 #include "Friends_List.h"
 #include "MapTiles_List.h"
 
-CFriends_Rancho::CFriends_Rancho() : m_pEAbilityL(0), m_pEAbilityR(0)
+#include "D3dDevice.h"
+
+CFriends_Rancho::CFriends_Rancho() : m_pEAbilityL(0), m_pEAbilityR(0),
+									 m_nEatFrame(0),
+									 m_Eat_Index(0, 0),
+									 m_AState(NONE), m_prevAState(NONE)
 {
 }
 CFriends_Rancho::~CFriends_Rancho()
@@ -21,7 +27,7 @@ CFriends_Rancho::~CFriends_Rancho()
 
 void CFriends_Rancho::Init()
 {
-	LoadDat("Resource/Data/Friends/Rancho.dat") ;
+	LoadRanchoDat() ;
 
 	m_pEAbilityL = new CEffect_RanchoEat ;
 	m_pEAbilityL->Init() ;
@@ -134,6 +140,181 @@ void CFriends_Rancho::Eat(int x, int y, char cDirection)
 			pEffect_RanchoEat->SetVisible(true) ;
 			pEffect_RanchoEat->SetPosition((x+direction) * 64.0f, y * 64.0f) ;
 			pFriend->SendEventMessage("RANCHO", pEffect_RanchoEat) ;
+
+			m_AState = EAT ;
 		}
 	}
+}
+
+void CFriends_Rancho::LoadRanchoDat()
+{
+	g_LoadManager->OpenDat("Resource/Data/Friends/Rancho.dat") ;
+
+	char item[100] ;
+	char image_path[100] ;
+
+	while(g_LoadManager->GetItem(item))
+	{
+		int len = strlen(item) ;
+
+		if(len==5 && strcmp(item, "IMAGE")==0)
+		{
+			g_LoadManager->GetString(image_path) ;
+		}
+		else if(len==4 && strcmp(item, "SIZE")==0)
+		{
+			g_LoadManager->GetValue(m_ImgSize.x) ;
+			g_LoadManager->GetValue(m_ImgSize.y) ;
+		}
+		else if(len==13 && strcmp(item, "COLLISION_BOX")==0)
+		{
+			g_LoadManager->GetValue(m_BoundingBox.left) ;
+			g_LoadManager->GetValue(m_BoundingBox.top) ;
+			g_LoadManager->GetValue(m_BoundingBox.right) ;
+			g_LoadManager->GetValue(m_BoundingBox.bottom) ;
+		}
+		else if(len==11 && strcmp(item, "STAND_FRAME")==0)
+		{
+			g_LoadManager->GetValue(m_nStandFrame) ;
+		}
+		else if(len==11 && strcmp(item, "STAND_INDEX")==0)
+		{
+			g_LoadManager->GetValue(m_Stand_Index.x) ;
+			g_LoadManager->GetValue(m_Stand_Index.y) ;
+		}
+		else if(len==12 && strcmp(item, "ABSORB_FRAME")==0)
+		{
+			g_LoadManager->GetValue(m_nAbsorbFrame) ;
+		}
+		else if(len==12 && strcmp(item, "ABSORB_INDEX")==0)
+		{
+			g_LoadManager->GetValue(m_Absorb_Index.x) ;
+			g_LoadManager->GetValue(m_Absorb_Index.y) ;
+		}
+		else if(len==13 && strcmp(item, "RELEASE_FRAME")==0)
+		{
+			g_LoadManager->GetValue(m_nReleaseFrame) ;
+		}
+		else if(len==13 && strcmp(item, "RELEASE_INDEX")==0)
+		{
+			g_LoadManager->GetValue(m_Release_Index.x) ;
+			g_LoadManager->GetValue(m_Release_Index.y) ;
+		}
+		else if(len==10 && strcmp(item, "STUN_FRAME")==0)
+		{
+			g_LoadManager->GetValue(m_nStunFrame) ;
+		}
+		else if(len==10 && strcmp(item, "STUN_INDEX")==0)
+		{
+			g_LoadManager->GetValue(m_Stun_Index.x) ;
+			g_LoadManager->GetValue(m_Stun_Index.y) ;
+		}
+		else if(len==9 && strcmp(item, "EAT_FRAME")==0)
+		{
+			g_LoadManager->GetValue(m_nEatFrame) ;
+		}
+		else if(len==9 && strcmp(item, "EAT_INDEX")==0)
+		{
+			g_LoadManager->GetValue(m_Eat_Index.x) ;
+			g_LoadManager->GetValue(m_Eat_Index.y) ;
+		}
+		else if(len==10 && strcmp(item, "ICON_INDEX")==0)
+		{
+			g_LoadManager->GetValue(m_Icon_Index.x) ;
+			g_LoadManager->GetValue(m_Icon_Index.y) ;
+		}
+	}
+
+	g_LoadManager->CloseDat() ;
+
+	m_pSprite = new CSprite ;
+	m_pSprite->Init((float)m_ImgSize.x, (float)m_ImgSize.y, image_path) ;
+	m_pSprite->SetTextureUV((float)(m_Stand_Index.x * m_ImgSize.x), (float)(m_Stand_Index.y * m_ImgSize.y),
+							(float)((m_Stand_Index.x+1) * m_ImgSize.x), (float)((m_Stand_Index.y+1) * m_ImgSize.y)) ;
+
+	SetBoundingBox() ;
+}
+
+void CFriends_Rancho::Animation()
+{
+	// Animation Frame, Index
+	int MaxFrame ;
+	Position Index ;
+
+	switch(m_State)
+	{
+	case STAND :
+		switch(m_AState)
+		{
+		case NONE :
+			MaxFrame = m_nStandFrame ;
+			Index = m_Stand_Index ;
+			break ;
+
+		case EAT :
+			MaxFrame = m_nEatFrame ;
+			Index = m_Eat_Index ;
+			break ;
+		}
+		break ;
+
+	case ABSORB :
+		MaxFrame = m_nAbsorbFrame ;
+		Index = m_Absorb_Index ;
+		break ;
+
+	case RELEASE :
+		MaxFrame = m_nReleaseFrame ;
+		Index = m_Release_Index ;
+		break ;
+
+	case STUN :
+		MaxFrame = m_nStunFrame ;
+		Index = m_Stun_Index ;
+		break ;
+	}
+
+	// Animation
+	m_fAnimationTime += g_D3dDevice->GetTime() ;
+
+	if(m_fAnimationTime>=0.1f || (m_State!=m_prevState) || (m_AState!=m_prevAState))
+	{
+		if(m_State!=m_prevState || m_AState!=m_prevAState)
+		{
+			m_nNowFrame = 0 ;
+			m_fAnimationTime = 0.0f ;
+		}
+
+		float left, top, right, bottom ;
+		left = (float)((Index.x + m_nNowFrame) * m_ImgSize.x) ;
+		top = (float)((Index.y) * m_ImgSize.y) ;
+		right = (float)((Index.x + m_nNowFrame+1) * m_ImgSize.x) ;
+		bottom = (float)((Index.y+1) * m_ImgSize.y) ;
+
+		m_pSprite->SetTextureUV(left, top, right, bottom) ;
+
+		int Frame = (int)(m_fAnimationTime / 0.1f) ;
+		m_fAnimationTime -= Frame * 0.1f ;
+		Frame %= MaxFrame ;
+		m_nNowFrame += Frame ;
+		if(m_nNowFrame>=MaxFrame)
+		{
+			if(m_State!=STUN)
+			{
+				// 흡수였을 경우, 캐릭터를 집어넣는다
+				if(m_State==ABSORB)
+					m_bUnVisible = true ;
+				m_nNowFrame = 0 ;
+				m_State = STAND ;
+
+				if(m_AState==EAT)
+					m_AState = NONE ;
+			}
+			else
+				m_nNowFrame = MaxFrame-1 ;
+		}
+	}
+
+	m_prevState = m_State ;
+	m_prevAState = m_AState ;
 }
--- Project-FW/Friends_Rancho.h
+++ Project-FW/Friends_Rancho.h
@@ -9,6 +9,12 @@ class CFriends_Rancho : public CFriends
 private :
 	CEffect_RanchoEat *m_pEAbilityL, *m_pEAbilityR ;
 
+	int m_nEatFrame ;
+	Position m_Eat_Index ;
+
+	enum Ability_State { NONE=0, EAT } ;
+	Ability_State m_AState, m_prevAState ;
+
 public :
 	CFriends_Rancho() ;
 	~CFriends_Rancho() ;
@@ -22,4 +28,8 @@ public :
 	void Render() ;
 private :
 	void Eat(int x, int y, char cDirection) ;
+	
+	void LoadRanchoDat() ;
+
+	void Animation() ;
 } ;
