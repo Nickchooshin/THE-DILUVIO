331a48dad4e21e25d9056e8de2cb71beb7663da4
Nickchooshin <gustncjswp95@gmail.com>
2015-02-11 17:32:31 +0900

    Credit 이미지 변경, 4-5 맵/프리뷰 변경

--- Project-FW/main.cpp
+++ Project-FW/main.cpp
@@ -25,7 +25,7 @@ INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR, INT )
 
 	g_SceneManager->StartScene(SceneLogo::scene()) ;
 
-	WinSystem.WinSet("THE DILUVIO - 1.00v (fixed)", WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX, 1024, 768) ;	// WS_THICKFRAME - 윈도우창 늘리기/줄이기 크기조정 지원안함.
+	WinSystem.WinSet("THE DILUVIO - 1.00v (fixed 2)", WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX, 1024, 768) ;	// WS_THICKFRAME - 윈도우창 늘리기/줄이기 크기조정 지원안함.
 	WinSystem.WinMsg() ;
 	WinSystem.WinEnd() ;
 
